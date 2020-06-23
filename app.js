2541064bc09258fcf5ef3ae807f214786207e5a9
Danilo Losi <danilo.lsanctis@hotmail.com>
2020-06-23 11:59:30 -0300

    Organizando estrutura

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const users = require('./routes/users')
 
 const app = express()
 
-app.set('views', path.join(__dirname), 'views')
+app.set('views', path.join(__dirname , 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 
