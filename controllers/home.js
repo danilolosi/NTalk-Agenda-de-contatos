3fd53a821ed7a29cfcf8492e4e8289a8c0d87cf0
Danilo Losi <danilo.lsanctis@hotmail.com>
2020-06-23 14:04:57 -0300

    home routes, controller e view

---
+++ controllers/home.js
@@ -0,0 +1,10 @@
+module.exports = (app) =>{
+    
+    const HomeController = {
+        index : function(req,res){
+            res.render('home/index')
+        }
+    }
+
+    return HomeController
+}
