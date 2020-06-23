e8f3b337b8b736a9f36cbe230f64d41098bd8cc5
Danilo Losi <danilo.lsanctis@hotmail.com>
2020-06-23 15:51:10 -0300

    CRUD contatos

--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,27 @@
 module.exports = (app) =>{
-    
+
     const HomeController = {
-        index : function(req,res){
+        index(req,res){
             res.render('home/index')
+        },
+
+        login(req,res){
+
+            const {usuario} = req.body
+            const {email, nome } = usuario
+
+            if(email && nome){
+                usuario.contatos = []
+                req.session.usuario = usuario
+                res.redirect('/contatos')
+            }else{
+                res.redirect('/')
+            }
+        },
+
+        logout(req,res){
+            req.session.destroy()
+            res.redirect('/')
         }
     }
 
---
+++ routes/contatos.js
@@ -0,0 +1,10 @@
+module.exports = (app) =>{
+    const {contatos} = app.controllers
+
+    app.get('/contatos', contatos.index)
+    app.get('/contato/:id', contatos.show)
+    app.post('/contato', contatos.create)
+    app.get('/contato/:id/editar', contatos.edit)
+    app.put('/contato/:id', contatos.update)
+    app.delete('/contato/:id', contatos.destroy)
+}
